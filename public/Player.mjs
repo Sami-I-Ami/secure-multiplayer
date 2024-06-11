6320aa448da1e97c3061805c0e587616739c6661
Sami Fink <fink.s.m.8651@gmail.com>
2024-06-11 13:45:38 +0000

    feat: completed classes for unit tests to run

--- public/Player.mjs
+++ public/Player.mjs
@@ -1,18 +1,38 @@
 class Player {
   constructor({x, y, score, id}) {
-
+    this.x = x;
+    this.y = y;
+    this.score = score;
+    this.id = id;
   }
 
   movePlayer(dir, speed) {
-
+    if (dir == "right") {
+      this.x += speed;
+    } else if (dir == "left") {
+      this.x -= speed;
+    } else if (dir == "up") {
+      this.y += speed;
+    } else if (dir == "down") {
+      this.y -= speed;
+    }
   }
 
   collision(item) {
-
+    if (this.x == item.x & this.y == item.y) {
+      return true
+    }
   }
 
   calculateRank(arr) {
-
+    const num_players = arr.length;
+    let rank = 1;
+    for (let player of arr) {
+      if (player.score > this.score) {
+        rank += 1;
+      }
+    }
+    return `Rank: ${rank}/${num_players}`;
   }
 }
 
